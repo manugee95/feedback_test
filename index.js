4593ddaa6c710372976d6d2bbeaecae7c2887f56
manugeetech <manugtech@gmail.com>
2024-01-30 14:26:08 +0100

    env

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 app = express();
 const cors = require("cors");
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/starkweb")
+  .connect(process.env.MONGO_DB_URI)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log(err));
 
 const user = require("./routes/user");
 const login = require("./routes/login");
 const feedback = require("./routes/feedback");
+
 
 app.use(express.json());
 app.use(
